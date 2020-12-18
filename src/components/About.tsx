9dee77ca68bc56bd4266dc8b94db7acd664db4cd
Sam-Macpherson <sam.macpherson15@gmail.com>
2020-12-18 16:44:07 -0500

    Add axios for institutions/domains, and toggling between login and register.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,72 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import { Paper } from "@material-ui/core"
 
+
+const useStyles = makeStyles(theme => ({
+  div: {
+    padding: theme.spacing(4, 4, 4, 4),
+  }
+}));
 
 function About() {
+  const styles = useStyles();
+
   return (
-    <div>
-      Indeed, a wise choice.
-    </div>
-  );
+    <Paper>
+      <div className={styles.div}>
+        <Grid item>
+          <Typography variant="h3">🦈 Sublet Shark</Typography>
+          <Divider />
+          <Typography variant="body1">
+            Sublet Shark is an application intended to provide a post-secondary student-centric 
+            sublet finding platform. In broad terms, Sublet Shark aims to eliminate the layers 
+            of landlords and leasing agents from the process of finding affordable, 
+            short-term housing for students in university towns. Sublet Shark is
+            developed by students, for students, with ease-of-use in mind.
+          </Typography>
+          <Typography variant="h5">
+            🎓 Emphasis on students
+          </Typography>
+          <Divider />
+          <Typography variant="body1">
+            Sublet Shark <strong>is</strong>:
+            <ul>
+              <li>For post-secondary students looking for short term accommodation near their institution</li>
+              <li>For post-secondary students looking to sublet their room/apartment to another local student</li>
+            </ul>
+            Sublet Shark <strong>is not</strong>:
+            <ul>
+              <li>For leases</li>
+              <li>For landlords or leasing agents, or anyone who isn't a student</li>
+              <li>For long-term rental arrangements</li>
+            </ul>
+          </Typography>
+          <Typography variant="h5">
+          ☑️ Commitment to quality
+          </Typography>
+          <Divider />
+          <Typography variant="body1">
+            The developers of Sublet Shark are students who have faced the housing
+            Facebook groups and the leasing sites term after term, and are sick of 
+            keeping track of listings on multiple platforms, while communicating 
+            with apathetic leasing agents and landlords through email. Account creation is 
+            exclusively available to students, and listings displayed on Sublet Shark 
+            are personally vetted, to ensure they are accurate and complete. We focus on 
+            the ease-of-use of our platform by building intuitive interfaces that compliment
+            our purpose.
+          </Typography>
+          <Divider />
+          <Typography variant="body2">
+            Sublet Shark is currently in development, but we hope to release something as soon as possible. 
+          </Typography>
+        </Grid>
+      </div>
+    </Paper>
+  )
 }
 
 export default About;
--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,21 +1,66 @@
 // The login page.
 
-import { Grid, makeStyles } from "@material-ui/core";
-import React from "react";
+import { Avatar, Button, Grid, Link, makeStyles, Paper, TextField, Typography } from "@material-ui/core";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import About from "../components/About";
-import Register from "../components/Form/Register";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
+import axios from "axios";
+import { Autocomplete } from "@material-ui/lab";
 
 const useStyles = makeStyles(theme => ({
   pageContainer: {
     height: "100%",
   },
   gridItem: {
-    padding: 2,
+    padding: theme.spacing(2),
   },
-}))
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    // width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+interface Institution {
+  domains: Array<string>;
+  name: string;
+  address: string;
+}
 
 function LoginPage() {
   const classes = useStyles();
+  const [registering, setRegistering] = useState(true);
+  const [institutions, setInstitutions] = useState([]);
+  const [chosenInstitution, setChosenInstitution] = useState<Institution | null>(null);
+  const [chosenDomain, setChosenDomain] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios.get(
+      '/institutions/institutions/', 
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        } 
+      }
+    ).then(response => {
+      setInstitutions(response.data);
+    }).catch(errors => {
+      console.log(errors);
+    })
+  }, []);
 
   return (
     <Grid 
@@ -23,14 +68,111 @@ function LoginPage() {
       alignItems="center"
       style={{ minHeight: '100%' }}
     >
-      <Grid item xs={8}>
+      <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
         <div className={classes.gridItem}>
           <About />
         </div>
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
         <div className={classes.gridItem}>
-          <Register />  
+          <Paper>
+            <div className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography variant="h5">{registering ? "Sign up" : "Log in"}</Typography>
+              <form className={classes.form} noValidate>
+                <Grid container spacing={2} alignItems="center">
+                  {registering && <Grid item xs={12}>
+                      <Autocomplete
+                        options={institutions}
+                        getOptionLabel={(institution: Institution) => institution.name}
+                        onChange={(event: ChangeEvent<{}>, value: Institution | null) => {
+                          setChosenInstitution(value);
+                          setChosenDomain('');
+                        }}
+                        renderInput={(params) => <TextField {...params} autoFocus label="Institution" variant="outlined" />}
+                      />
+                  </Grid>}
+                  {registering && <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="fname"
+                      name="firstName"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                    />
+                  </Grid>}
+                  {registering && <Grid item xs={12} sm={6}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="lname"
+                    />
+                  </Grid>}
+                  <Grid item xs={registering ? 5 : 12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  {registering && <Grid item xs={1}>
+                    <Typography align="center" variant="h5">@</Typography>
+                  </Grid>}
+                  {registering && <Grid item xs={6}>
+                    <Autocomplete
+                      options={chosenInstitution ? chosenInstitution.domains : []}
+                      getOptionLabel={(domain: string) => domain}
+                      value={chosenDomain}
+                      onChange={(event: ChangeEvent<{}>, value: string | null) => {
+                        setChosenDomain(value);
+                      }}
+                      renderInput={(params) => <TextField {...params} variant="outlined" />}
+                    />
+                  </Grid>}
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  {registering ? "Sign up" : "Log in"}
+                </Button>
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Link href="#" variant="body2" onClick={() => {setRegistering(!registering)}}>
+                      {registering ? "Already have an account? Log in" : "Don't have an account? Sign up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
+          </Paper>
         </div>
       </Grid>
     </Grid>
